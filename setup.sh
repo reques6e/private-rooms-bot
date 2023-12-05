9bd298905de9b9cda8e8e1520c5d8a94626ac1cf
Alexander <149709871+reques6e@users.noreply.github.com>
2023-12-05 22:37:42 +0300

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+SERVICE_FILE="/etc/systemd/system/privateroomsbot.service"
+
+SERVICE_CONTENT="[Unit]
+Description=TicketBot
+After=network.target
+
+[Service]
+Type=simple
+WorkingDirectory=$SCRIPT_PATH
+ExecStart=/usr/bin/python3 $SCRIPT_PATH/main.py
+Restart=always
+
+[Install]
+WantedBy=multi-user.target"
+
+echo "$SERVICE_CONTENT" | sudo tee "$SERVICE_FILE" > /dev/null
+
+sudo systemctl start profileactivity.service
+
+if [ $? -eq 0 ]; then
+    echo "Установка успешно завершена. Служба profileactivity успешно запущена.."
+    sudo systemctl status profileactivity.service
+    echo "github.com/reques6e/private-rooms-bot - privateroomsbot"
+else
+    echo "Произошла ошибка при установке."
+fi
